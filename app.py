93313a54ef09d8d59640e377d8ad2be748302f7b
Raj Rehal <raj__kaur@hotmail.com>
2021-04-23 22:41:07 +0100

    Login, add blog, flash messages

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import initial
 from dbsetup import Base, Users, BlogPosts
 from werkzeug.security import generate_password_hash, check_password_hash
 import datetime
-
+from flask_login import LoginManager
+from flask_login import login_user, login_required, logout_user, current_user
 today = datetime.datetime.today()
 
 engine = create_engine("mysql+mysqlconnector://admin2:@GitPa$$w0rd#@54.74.234.11/thefantasticfour?charset=utf8mb4")
@@ -13,6 +14,14 @@ DBSession = orm.sessionmaker(bind=engine)
 session = DBSession()
 
 app = initial.create_app()
+
+login_manager = LoginManager()
+login_manager.login_view = 'app.login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(id):
+    return engine.execute(f"SELECT user_id FROM users WHERE user_id = '{id()}';")
 
 
 @app.route('/home')
@@ -32,58 +41,103 @@ def projects():
     return render_template('projects.html')
 
 
-@app.route('/post', methods=['POST'])
+@app.route('/post', methods=['GET'])
 def post():
     return render_template('blog_post2.html')
 
 
-@app.route('/subscribe',methods=['GET', 'POST'])
+# @app.route('/subscribe',methods=['GET', 'POST'])
+# def subscribe():
+#     if request.method == 'POST':
+#         new_signup = Users(user_name=request.form['firstName'], user_email=request.form['email'],
+#                        user_pw=generate_password_hash(request.form['password1']))
+#         session.add(new_signup)
+#         session.commit()
+#         return render_template("subscribe.html")
+#     else:
+#         return render_template("subscribe.html")
+
+
+@app.route('/subscribe', methods=['GET', 'POST'])
 def subscribe():
+    # flash('It worked', category='success')
     if request.method == 'POST':
-        new_signup = Users(user_name=request.form['firstName'], user_email=request.form['email'],
-                       user_pw=generate_password_hash(request.form['password1']))
-        session.add(new_signup)
-        session.commit()
-        return render_template("subscribe.html")
-    else:
-        return render_template("subscribe.html")
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        user = engine.execute(f"SELECT user_email FROM users WHERE user_email = '{email}';").first()
+        if user:
+            flash('Email already exists.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        else:
+            new_user = Users(user_name=request.form['firstName'], user_email=request.form['email'],
+                        user_pw=generate_password_hash(request.form['password1']))
+            session.add(new_user)
+            session.commit()
+            # login_user(new_user, remember=True)
+            flash('Account created!', category='success')
+    return render_template("subscribe.html")
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-
-
-    if form.validate_on_submit():
+    if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
         user = engine.execute(f"SELECT user_email FROM users WHERE user_email = '{email}';").first()
         pwdata = engine.execute(f"SELECT user_pw FROM users WHERE user_email = '{email}';").first()
-        if not user:
-            flash("Invalid Credentials, Please Try Again")
-        else:
+        if user:
             for pw in pwdata:
                 if check_password_hash(pw, password):
-                    print("TADA!")
-                    return render_template('home.html')
-                else:
-                    flash("Invalid Credentials, Please Try Again")
-                    return render_template("login.html", form=form)
-    return render_template('login2.html', form=form)
+                    flash('Logged in successfully!', category='success')
+                    user=login_user(remember=True)
+                return render_template("index.html")
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
 
+    return render_template("login2.html", user=current_user)
 
-@app.route('/blog_post', methods=['GET', 'POST'])
+@app.route('/logout')
+# @login_required
+def logout():
+    logout_user()
+    flash('You have successfully logged out!', category='success')
+    return render_template("index.html")
+
+@app.route('/add_blog', methods=['GET', 'POST'])
+# @login_required
 def newpost():
-    form = BlogPost()
+        form = BlogPost()
+        if request.method == 'POST':
+            newpost = request.form.get('blog_post')
+            # if len(newpost) < 1:
+            #     flash('Blog post is too short!', category='error')
+            #     return render_template('add_blog.html')
+            if form.validate_on_submit():
+                title = form.title.data
+                content = form.content.data
+                new_blog = BlogPosts(post_title=title, post_content=content, post_date=today)
+                session.add(new_blog)
+                session.commit()
+                flash("Blog post added")
+            return render_template('index.html')
 
-    if form.validate_on_submit():
-        title = form.title.data
-        content = form.content.data
-        new_blog = BlogPosts(post_title=title, post_content=content, post_date=today)
-        session.add(new_blog)
-        session.commit()
-        flash("Blog post added")
-        return render_template('home.html')
-    return render_template('blog_post.html', form=form)
+        return render_template("add_blog.html", form=form, user=current_user)
+    # form = BlogPost()
+    #
+    # if form.validate_on_submit():
+    #     title = form.title.data
+    #     content = form.content.data
+    #     new_blog = BlogPosts(post_title=title, post_content=content, post_date=today)
+    #     session.add(new_blog)
+    #     session.commit()
+    #     flash("Blog post added")
+    #     return render_template('index.html')
+    # return render_template('add_blog.html', form=form)
 
 @app.route('/contact')
 def contact():
